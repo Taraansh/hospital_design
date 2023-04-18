b29c29a4b2dd4c18a064a2d702b651023a4c244e
Taraansh <taraanshnandwani123@gmail.com>
2023-04-18 23:49:00 +0530

    Added paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Login from './components/Login';
-import Home from './components/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Login from "./components/Login";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/Home" element={<Home/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
