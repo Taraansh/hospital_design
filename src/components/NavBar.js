74c89285ec65579ab8d8c3e58e745e5afc8ef4e3
Taraansh <taraanshnandwani123@gmail.com>
2023-05-06 23:42:27 +0530

    Navbar is done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function NavBar() {
+    const navigate = useNavigate();
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/Home">Navbar</Link>
+                <Link className="navbar-brand" to="/Appointment">Hospital</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" to="/Home">Home</Link>
+                            <Link className="nav-link" to="/Appointment">Appointments</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Appointments</Link>
+                            <Link className="nav-link" to="/Tests">Tests</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/#">Tests</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Medicines</Link>
+                            <Link className="nav-link" to="/Orders">Medicines</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/About">Profile</Link>
@@ -30,9 +28,8 @@ export default function NavBar() {
                     </ul>
                 </div>
             </div>
-            {/* <Linkbout/> */}
 
-            <button className="btn btn-dark mx-2">Logout</button>
+            <button className="btn btn-dark mx-2" onClick={()=> {navigate('/')}}>Logout</button>
         </nav>
     </div>
   )
